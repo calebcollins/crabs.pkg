e1b414e17e91af56d1fdb9954b52df16c2800861
Caleb Collins <caleb.collins-2@selu.edu>
2023-11-15 00:14:36 +0000

    update function 3

--- R/lm_function.R
+++ R/lm_function.R
@@ -1,7 +1,18 @@
-lm_function <- function(var, column, column_2){
-  model<-var%>% 
+#' Calculate volume using H*L*W
+#' 
+#' Using Height, Length and Width to calculate volume by multiplying the three number 
+#' 
+#' @param df data set being used
+#' @param column x in the lm 
+#' @param column_2 y in the lm 
+#' @return Linear model chart  
+#' 
+#' @export
+
+lm_function <- function(df, column, column_2){
+  model<-df%>% 
     select({{column}},{{column_2}})
-    lm({{column}} ~ {{column_2}}, data = var)
+    lm({{column}} ~ {{column_2}}, data = df)
   return(model)
 }
 #lm_function(crabs, rear_width, body_depth)
